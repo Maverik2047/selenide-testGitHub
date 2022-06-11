934170d62bb13ecd94c2dd577388ea2ef64dfe84
Viacheslav <maverik2047@yandex.ru>
2022-06-11 20:49:23 +0300

    softAssertionsShouldHaveJUnit5Example test added

--- src/test/java/github/SelenideRepositoryTest.java
+++ src/test/java/github/SelenideRepositoryTest.java
@@ -1,15 +1,17 @@
 package github;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
-public class SelenideRepositoryTest extends TestBase{
+public class SelenideRepositoryTest extends TestBase {
 
     @Test
-    void findSelenidePageInGithub() {
+    void findSoftAssertions() {
 //        Откройте страницу Selenide в Github
 //        Перейдите в раздел Wiki проекта
 //        Убедитесь, что в списке страниц(Pages) есть страница SoftAssertions
@@ -19,8 +21,15 @@ public class SelenideRepositoryTest extends TestBase{
         $(".form-control").setValue("Selenide").pressEnter();
         $$(".repo-list").first().$("a").click();
         $("#wiki-tab").click();
+        $(".Box-row.wiki-more-pages-link").$(byText("Show 2 more pages…")).click();
+        $(".application-main").shouldHave(Condition.text("SoftAssertions"));
 
+    }
 
+    @Test
+    void softAssertionsShouldHaveJUnit5Example() {
+        Selenide.open("https://github.com/selenide/selenide/wiki/SoftAssertions");
+        $(".repository-content").shouldHave(Condition.text("3. Using JUnit5 extend test class:"));
 
 
     }
